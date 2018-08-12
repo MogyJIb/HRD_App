13cba34247124a52311af1a80aa474ea53b636d8
Erika <erikamatveenko@gmail.com>
2018-08-13 00:05:57 +0300

    Add api exceptions

---
+++ HRD_App/Errors/ApiException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRD_DataLibrary.Errors;
+
+namespace HRD_App.Errors
+{
+    public class ApiException : Exception
+    {
+        public ErrorType ErrorType { get; }
+
+        public ApiException(ErrorType errorType) : base() { }
+        
+        public override string Message => "Default API exception";
+
+        public static ApiException Create(ErrorType errorType)
+        {
+            switch(errorType)
+            {
+                case ErrorType.WrongPassword:
+                    return new WrongPasswordException(errorType);
+                case ErrorType.NonExistentLogin:
+                    return new NonExistentLoginException(errorType);
+                default:
+                    return new ApiException(errorType);
+            }
+        }
+    }
+}
---
+++ HRD_App/Errors/NonExistentLoginException.cs
@@ -0,0 +1,16 @@
+﻿using HRD_DataLibrary.Errors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRD_App.Errors
+{
+    class NonExistentLoginException : ApiException
+    {
+        public NonExistentLoginException(ErrorType errorType) : base(errorType) { }
+
+        public override string Message => "Такого логина не существует";
+    }
+}
