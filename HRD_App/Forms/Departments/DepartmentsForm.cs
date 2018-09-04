ef7b22ee2ad16b3996f801157e8ae54903f02616
Evgeniy Shevtsov <eshevtsov@intervale.ru>
2018-09-05 00:35:43 +0300

    Refactor get departmens request

--- HRD_App/Forms/Departments/DepartmentsForm.cs
+++ HRD_App/Forms/Departments/DepartmentsForm.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HRD_App.Errors;
+using HRD_App.Rest;
+using HRD_DataLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,6 +20,25 @@ namespace HRD_App.Forms
             InitializeComponent();
 
             new AuthorizationForm().ShowDialog();
+
+            init();
+        }
+
+        private async void init()
+        {
+            try
+            {
+                var departments = await RestApi.DepartmentService.GetDepartments();
+                MessageBox.Show(string.Join<Department>(",  ", departments.ToArray()));
+            }
+            catch(ApiException exception)
+            {
+                MessageBox.Show(exception.ErrorType + "    " + exception.Message);
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
         }
     }
 }
--- HRD_App/Rest/Services/DepartmentService.cs
+++ HRD_App/Rest/Services/DepartmentService.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using HRD_App.Errors;
+using HRD_App.Utils;
+using HRD_DataLibrary.Errors;
 using HRD_DataLibrary.Models;
 
 namespace HRD_App.Rest
@@ -12,27 +15,27 @@ namespace HRD_App.Rest
     {
         private const string NAME = "departments";
         private HttpClient httpClient;
+        private Dictionary<string, string> args;
 
-        public DepartmentService(HttpClient httpClient)
+        public DepartmentService(HttpClient httpClient, Dictionary<string, string> args)
         {
             this.httpClient = httpClient;
+            this.args = args;
         }
 
         public async Task<List<Department>> GetDepartments()
         {
-           
-            List<Department> departments = new List<Department>();
-            HttpResponseMessage response = await httpClient.GetAsync(NAME);
-
-            //log
-            Console.WriteLine(response.StatusCode);
-            Console.WriteLine(response.Content.ToString());
+            HttpResponseMessage response = await httpClient.GetAsync(NAME + "?" + UrlArgsUtil.makeArgs(args));
 
             if (response.IsSuccessStatusCode)
             {
-                departments = await response.Content.ReadAsAsync<List<Department>>();
+                return await response.Content.ReadAsAsync<List<Department>>();
             }
-            return departments;
+            else
+            {
+                ErrorType errorType = await response.Content.ReadAsAsync<ErrorType>();
+                throw ApiException.Create(errorType);
+            }
         }
     }
 }
